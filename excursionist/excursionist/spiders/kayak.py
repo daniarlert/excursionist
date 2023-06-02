8fe712ab239c54680e651e328043afa6db31e31f
daniarlert <arlertdaniel@gmail.com>
2023-06-02 16:38:42 +0200

    feat(spider): add spider to kayak with destination

--- excursionist/excursionist/spiders/kayak.py
+++ excursionist/excursionist/spiders/kayak.py
@@ -1,5 +1,4 @@
 import os
-from datetime import datetime, timedelta
 
 from dotenv import load_dotenv
 from excursionist.items import OfferItem
@@ -18,7 +17,7 @@ def gen_url(
     if not destination_city:
         return f"https://www.kayak.com/explore/{origin_city}-anywhere/{start_date.replace('-', '')},{end_date.replace('-', '')}"
     else:
-        return f"https://www.kayak.com/flights/{origin_city}-{destination_city}/{start_date}/{end_date}/?sort=bestflight_a"
+        return f"https://www.kayak.com/flights/{origin_city}-{destination_city}/{start_date}/{end_date}"
 
 
 class KayakSpider(Spider):
@@ -73,7 +72,6 @@ class KayakSpider(Spider):
                     item["country"] = offer.css("div.Country__Name::text").get()
                     item["city"] = offer.css("div.City__Name::text").get()
                     item["price"] = offer.css("div.City__Name + div::text").get()
-                    item["timestamp"] = datetime.utcnow().isoformat()
                     item["start_date"] = self.start_date
                     item["end_date"] = self.end_date
                     item["travel_page"] = "kayak"
@@ -81,9 +79,79 @@ class KayakSpider(Spider):
 
                     yield item
 
-                break  # Break out of the while loop
+                break  # Break while loop
             else:
                 await page.click('button[id$="showMoreButton"]')
+                await page.wait_for_timeout(1000)  # Adjust the delay if necessary
+
+                # Update the response object with the new HTML. Since we're not using the normal Scrapy
+                # flow, we need to do this manually.
+                updated_html = await page.content()
+                response = response.replace(body=updated_html)
+
+        await page.close()
+
+    async def errback(self, failure):
+        page = failure.request.meta["playwright_page"]
+        await page.close()
+
+
+class KayakSpider(Spider):
+    name = "kayak"
+    allowed_domains = ["www.kayak.com"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.origin_city = os.getenv("ORIGIN_CITY")
+        self.destination_city = os.getenv("DESTINATION_CITY")
+        self.start_date = os.getenv("START_DATE")
+        self.end_date = os.getenv("END_DATE")
+        self.max_requests = int(os.getenv("MAX_REQUESTS", 1))
+
+        if not self.origin_city:
+            raise ValueError("ORIGIN_CITY is not set.")
+        if not self.destination_city:
+            raise ValueError("DESTINATION_CITY is not set.")
+        if not self.start_date:
+            raise ValueError("START_DATE is not set.")
+        if not self.end_date:
+            raise ValueError("END_DATE is not set.")
+
+    def start_requests(self):
+        url = gen_url(
+            self.origin_city,
+            self.destination_city,
+            self.start_date,
+            self.end_date,
+        )
+
+        yield Request(
+            url,
+            meta={
+                "playwright": True,
+                "playwright_include_page": True,
+                "playwright_page_methods": [
+                    PageMethod("wait_for_selector", "div.resultsList"),
+                ],
+                "errback": self.errback,
+            },
+        )
+
+    async def parse(self, response):
+        page = response.meta["playwright_page"]
+        num_requests = 0
+
+        while True:
+            load_more_button = await page.query_selector("div.show-more-button")
+            is_hidden = await load_more_button.is_hidden()
+
+            if is_hidden or not load_more_button or num_requests >= self.max_requests:
+                for offer in response.css("div.Explore-resultsList"):
+                    pass
+
+                break  # Break while loop
+            else:
+                await page.click("div.show-more-button")
                 await page.wait_for_timeout(1000)  # Adjust the delay if necessary
 
                 # Update the response object with the new HTML. Since we're not using the normal Scrapy

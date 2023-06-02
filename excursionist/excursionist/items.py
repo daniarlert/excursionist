2f78f214ad519b91c3d98f3dd7334447eb7e8223
daniarlert <arlertdaniel@gmail.com>
2023-06-02 16:35:19 +0200

    feat(models): add new fields to the OfferModel
    Add new fields to the OfferModel to handle information such as the airline.

--- excursionist/excursionist/items.py
+++ excursionist/excursionist/items.py
@@ -10,19 +10,23 @@ class OfferItem(Item):
         country (Field): Destination country of the travel offer.
         city (Field): Destination city of the offer.
         price (Field): Price of the offer in US dollars.
-        timestamp (Field): Timestamp of when the offer was scraped.
         start_date (Field): Start date of the travel.
         end_date (Field): End date of the travel.
+        departure_time (Field): Departure time of the flight.
+        arrival_time (Field): Arrival time of the flight.
         travel_page (Field): Name of the travel page where the offer was scraped.
         url (Field): URL of the offer page, or in some cases, the URL of the offers page.
+        airline (Field): Name of the airline.
     """
 
     origin = Field()
     country = Field()
     city = Field()
     price = Field()
-    timestamp = Field()
     start_date = Field()
     end_date = Field()
+    departure_time = Field()
+    arrival_time = Field()
     travel_page = Field()
     url = Field()
+    airline = Field()
--- excursionist/excursionist/models.py
+++ excursionist/excursionist/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -13,11 +15,17 @@ class OfferModel(Base):
     country: Mapped[str] = mapped_column(nullable=False)
     city: Mapped[str] = mapped_column(nullable=False)
     price: Mapped[float] = mapped_column(nullable=False)
-    timestamp: Mapped[str] = mapped_column(nullable=False)
+    timestamp: Mapped[datetime] = mapped_column(
+        nullable=False,
+        default=datetime.utcnow(),
+    )
     start_date: Mapped[str] = mapped_column(nullable=False)
     end_date: Mapped[str] = mapped_column(nullable=True)
+    departure_time: Mapped[datetime] = mapped_column(nullable=True)
+    arrival_time: Mapped[datetime] = mapped_column(nullable=True)
     travel_page: Mapped[str] = mapped_column(nullable=False)
     url: Mapped[str] = mapped_column(nullable=False)
+    airline: Mapped[str] = mapped_column(nullable=True)
 
     def __repr__(self):
-        return f"<Offer(origin={self.origin}, country={self.country}, city={self.city}, price={self.price}, timestamp={self.timestamp}, start_date={self.start_date}, end_date={self.end_date}, travel_page={self.travel_page}, url={self.url})>"
+        return f"<OfferModel(origin={self.origin}, country={self.country}, city={self.city}, price={self.price}, timestamp={self.timestamp}, start_date={self.start_date}, end_date={self.end_date}, departure_time={self.departure_time}, arrival_time={self.arrival_time}, travel_page={self.travel_page}, url={self.url}, airline={self.airline})>"
